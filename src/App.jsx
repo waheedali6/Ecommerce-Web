8711a19c4f472cf1a1ed3bfb19ff1994918bab2c
waheedali6 <159957713+waheedali6@users.noreply.github.com>
2026-01-15 20:01:28 +0500

    installed some packages and basic setup

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar'
+import Example from './components/Navbar'
 
 function App() {
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./navbar.css"
-import {} from 
-import { TextField, ToggleButton } from '@mui/material'
-import { IoSearchOutline } from "react-icons/io5";
+// import {Toggle} from "reactstrap"
+import { Button, Slide, TextField } from '@mui/material'
+import SearchIcon from '@mui/icons-material/Search';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 
 const Navbar = () => {
+    const [isSearch, setSearch] = useState(false);
     return (
         <nav className="@container">
             <div className="grid grid-cols-12 md:px-20 pt-4">
@@ -22,19 +24,24 @@ const Navbar = () => {
                 </div>
                 <div className="md:col-span-4 col-span-12 flex justify-end">
                     <div>
-                        <div className="flex items-center">
-                            <IoSearchOutline />
-                            <h1>Search</h1>
+                        <div className="flex items-center p-2 cursor-pointer" onClick={() => setSearch(!isSearch)}>
+                            <SearchIcon className="text-2xl"/>
                         </div>
-                        <div>
-                            <ToggleButton>
-<Drop
-                            <TextField className="search-bar" placeholder="Search" size="small" variant="outlined" />
-                            </ToggleButton>
+                        <div className="absolute">
+                            <Slide direction="up" in={isSearch} mountOnEnter unmountOnExit>
+                                <TextField className="search-bar" placeholder="Search" size="small" variant="outlined" />
+                            </Slide>
                         </div>
 
                     </div>
-                    <div>Profile</div>
+                    <div>
+                        <Button size='small'>Sign In</Button>
+                        <Button size='small'>Sign Up</Button>
+                    </div>
+                    <div>
+                        <AccountCircleIcon className="text-2xl"/>
+
+                    </div>
                 </div>
             </div>
         </nav>
